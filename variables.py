3875ebacac612f9637c3655140f63fafa3ceb84f
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-05-13 17:13:16 +0545

    datatypes in python list,tuple,dictionaries etc

--- variables.py
+++ variables.py
@@ -13,7 +13,17 @@ print("The type of variable b is ",type(b))
 print("The type of variable c is ",type(c))
 print("The type of variable d is ",type(d))
 
+#List 
+list1=[1,2,[5,6],["apple","mango"]]
+print(list1)
 
+#tuple
+tuple1=(("Birds","Parrot"),("Second","Third"),(5,6))
+print(tuple1)
+
+#Dictionaries
+dict1={"Name ":"Basanta", "Age":"22", "Level":"6"}
+print(dict1)
 
 
 
