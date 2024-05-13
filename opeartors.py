463f50929cdad9346feaff968a2db3145848159a
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-05-13 17:24:19 +0545

    Arithmetic opeartors in python

---
+++ opeartors.py
@@ -0,0 +1,7 @@
+print(15+3) #Addition operator
+print(15-3) #Subtraction operator
+print(15*3) # Multiplication operator
+print(15/3) #Division opeartor
+print(15//3) # Floor division operator
+print(15%3) # Moduus operator
+print(15**3) # Exponential operator
