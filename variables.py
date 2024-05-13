b324569d46570060c4e9080982fd812e711dd007
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-05-13 17:06:14 +0545

    file for variables with its type and all

---
+++ variables.py
@@ -0,0 +1,21 @@
+a=1
+b=2.2
+c=True
+d="Hello"
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print("The type of variable a is ",type(a))
+print("The type of variable b is ",type(b))
+print("The type of variable c is ",type(c))
+print("The type of variable d is ",type(d))
+
+
+
+
+
+
+
