00603b876b84133151ee3f5709d7e6919b407a0f
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-05-13 16:56:35 +0545

    About print statement escape sequ and  comments

--- hello.py
+++ hello.py
@@ -1,3 +1,32 @@
 print("Hello Everyone!!.")
 print(19)
 print(19*20)
+
+# This is my first Comment in python
+'''
+Commenting multiline in python using tripple single quotes.
+'''
+
+#Also we can do it by triple double quotes as well.
+
+"""
+Commenting multiline in python using tripple double quotes.
+"""
+
+#escape sequence character in python(\n)
+print("Hello this is first line \nand this is the second line.")
+
+#we can add symbols in the print statements by using \ like below
+
+print("I am putting \" by using \\ this symbol known as escape sequence character.")
+
+print("We can print multiple values in the print statement like below.")
+
+print("1st value",2,"3rd value")
+
+#We can seperate the multiple values using sep inside the print statement like
+print("Hello","is seperated by ~ sign",sep="~")
+#also has a end parameter which is use to enf the print with something by default 
+#The value will be \n but if we put something we forcefully need to put \n
+
+print("Hello",2,"By", sep="~",end=" Have a great day\n")
